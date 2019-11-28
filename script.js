2ae86c681befba8f9d63b9dde5465f004ce86ec3
beenish <beeni.khurram@gmail.com>
2019-11-28 18:17:22 +0000

    added index.html and script.js

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 //display current date on page
 var date = new Date(); 
 var dateOnPage= document.getElementById("current-date");
+
 dateOnPage.textContent =date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear();
 
  $(document).ready(function() {
  $(".save-button").click(function() {
- console.log("hello");
+     var $text = $("#task-17")
+     var $time = $("#time-9") 
+     //console.log($time.textContent);
+     var $time =  $(this).parent().attr("id");
+     
+    localStorage.setItem($time, $text.val());
+     console.log("hello");
  })
  })
