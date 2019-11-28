63ec0f12c69c80f7bef7722588570a749c3ccc76
beenish <beeni.khurram@gmail.com>
2019-11-28 10:34:24 +0000

    added index.html

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+//display current date on page
 var date = new Date(); 
 var dateOnPage= document.getElementById("current-date");
 dateOnPage.textContent =date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear();
+
+ $(document).ready(function() {
+ $(".save-button").click(function() {
+ console.log("hello");
+ })
+ })
