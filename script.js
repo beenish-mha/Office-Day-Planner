a6fd37207aa1715ca807d599af24bab23133e991
beenish <beeni.khurram@gmail.com>
2019-11-29 10:40:47 +0000

    added script.js

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ dateOnPage.textContent =date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear(
  $(document).ready(function() {
      var $time;
      var $text;
+     var $name = $("#name")
+   
      //save at local storage
     $(".save-button").click(function() {
-     
+        localStorage.setItem("name",$name.val())
       $time =  $(this).parent().attr("id"); 
       if ($time === "5PM") {
           $text = $("#task-17");
@@ -27,7 +29,7 @@ dateOnPage.textContent =date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear(
       else if ($time === "1PM") {
          $text = $("#task-13");
       }
-      else if ($time === "12 Noon") {
+      else if ($time === "Noon") {
          $text = $("#task-12");
       }
       else if ($time === "11AM") {
@@ -44,22 +46,16 @@ dateOnPage.textContent =date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear(
     })
 
 // getting data from local storage and displaying on textarea
+   
+   $("#name").val(localStorage.getItem("name"));
    $("#task-9").val(localStorage.getItem("9AM"));
    $("#task-10").val(localStorage.getItem("10AM"));
    $("#task-11").val(localStorage.getItem("11AM"));
-   $("#task-12").val(localStorage.getItem("12 Noon"));
+   $("#task-12").val(localStorage.getItem("Noon"));
    $("#task-13").val(localStorage.getItem("1PM"));
    $("#task-14").val(localStorage.getItem("2PM"));
    $("#task-15").val(localStorage.getItem("3PM"));
    $("#task-16").val(localStorage.getItem("4PM"));
-   $("#task-17").val(localStorage.getItem("5PM"));
-     
-   
-        
-    // var $displayTask= $("#task-9");
-    
-    // var $savedData = localStorage.getItem("9AM");
-    // $displayTask.val($savedData);
-    
+   $("#task-17").val(localStorage.getItem("5PM"));  
  })
  
