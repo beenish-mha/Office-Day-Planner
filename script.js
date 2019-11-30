b62eafb82d17cc0d1fd28a61bced624f85cfeecc
beenish <beeni.khurram@gmail.com>
2019-11-30 19:44:48 +0000

    added in script.js

--- script.js
+++ script.js
@@ -2,21 +2,41 @@
 //display current date on page
 $dateOnPage = $("#current-date");
 $dateOnPage.text(moment().format('dddd    DD/ MM/ YYYY'));
+$currentHour = parseInt(moment().format("HH"));
+console.log($currentHour);
 
-//var date = new Date(); 
-//var dateOnPage= document.getElementById("current-date");
+//get the id of each row to compare with current time
+var $timeArray = [];
+$("#main-section").find("tr").each(function(){
+    $timeArray.push($(this).attr("id")); 
+   });
+//console.log($timeArray);
+var $numb;
 
-//dateOnPage.textContent =date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear();
+//change the color of textcontent according to the current time
+for( var i = 0; i< $timeArray.length; i++){
+   $numb = parseInt($timeArray[i]);
+  if ($currentHour > $numb){
+      console.log("past ");
+  } 
+  else if ($currentHour < $numb) {
+      console.log("future");
+   }
+  else if ($currentHour === $numb) {
+      console.log("present");
+   }
 
+}
  $(document).ready(function() {
      var $time;
      var $text;
-       
+
+
      //save at local storage
     $(".save-button").click(function() {
-        
       $time =  $(this).parent().attr("id"); 
       if ($time === "5PM") {
+         console.log($time)
           $text = $("#task-17");
       }
       else if ($time === "4PM") {
@@ -31,7 +51,7 @@ $dateOnPage.text(moment().format('dddd    DD/ MM/ YYYY'));
       else if ($time === "1PM") {
          $text = $("#task-13");
       }
-      else if ($time === "Noon") {
+      else if ($time === "NOON") {
          $text = $("#task-12");
       }
       else if ($time === "11AM") {
@@ -53,7 +73,7 @@ $dateOnPage.text(moment().format('dddd    DD/ MM/ YYYY'));
    $("#task-9").val(localStorage.getItem("9AM"));
    $("#task-10").val(localStorage.getItem("10AM"));
    $("#task-11").val(localStorage.getItem("11AM"));
-   $("#task-12").val(localStorage.getItem("Noon"));
+   $("#task-12").val(localStorage.getItem("NOON"));
    $("#task-13").val(localStorage.getItem("1PM"));
    $("#task-14").val(localStorage.getItem("2PM"));
    $("#task-15").val(localStorage.getItem("3PM"));
